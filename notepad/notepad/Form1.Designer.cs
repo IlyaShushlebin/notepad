256a38c9f88f5b0627029348bd625f3ce9484de7
IlyaShushlebin <43331073+IlyaShushlebin@users.noreply.github.com>
2019-11-18 19:50:52 +0700

    Added RegularExpressions Numbers, InvertedСommas, Comments, Tags

--- notepad/notepad/Form1.Designer.cs
+++ notepad/notepad/Form1.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.menuStrip1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.accountToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1008, 26);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(49, 22);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(52, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // accountToolStripMenuItem
+            // 
+            this.accountToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.accountToolStripMenuItem.Name = "accountToolStripMenuItem";
+            this.accountToolStripMenuItem.Size = new System.Drawing.Size(84, 22);
+            this.accountToolStripMenuItem.Text = "Account";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 26);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1008, 703);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.richTextBox1_KeyUp);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accountToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- notepad/notepad/Form1.cs
+++ notepad/notepad/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace notepad
 {
@@ -15,6 +16,111 @@ namespace notepad
         public Form1()
         {
             InitializeComponent();
+
+            richTextBox1.SelectionIndent = 5;
+            richTextBox1.SelectionRightIndent = 5;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private string RegularExpressionsNumbers(string str)
+        {
+            string s = "";
+            Regex regex = new Regex(@"\d");
+            MatchCollection matchCollection = regex.Matches(str);
+            if (matchCollection.Count > 0)
+            {
+                foreach(Match key in matchCollection)
+                {
+                    s += key.Value;                    
+                }
+            }
+            return s;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {            
+            //string regaxAll = @"\w\W*";
+            string regaxNumbers = @"\b\d+(\.|,\d*)?\b";
+            string regaxTags = @"</?[^>]+>";
+            string regaxInvertedСommas = @"\""((?=[\w\W])[^\""])*\""";//кавычки
+            string regaxOneLineComment = @"//.*";
+            string regaxMultilineComment = @"/\*((?=[\w\W])[^/\*])*\*/";
+
+            string regaxDate = @"\d{2}(\.|/|-)\d{2}\1\d{4}";
+            //Построй соотношение слов с цветом, которым хочешь их выделить. Выделять будем все слова независимо от регистра
+            KeyValuePair<Regex, Color>[] PAIRS = new KeyValuePair<Regex, Color>[]
+            {
+                //new KeyValuePair<Regex, Color>(new Regex(regaxAll, RegexOptions.IgnoreCase), Color.FromArgb(255,255,255)),
+                new KeyValuePair<Regex, Color>(new Regex(regaxNumbers, RegexOptions.IgnoreCase), Color.FromArgb(151,255,168)),
+                new KeyValuePair<Regex, Color>(new Regex(regaxTags, RegexOptions.IgnoreCase), Color.FromArgb(255,179,52)),
+                new KeyValuePair<Regex, Color>(new Regex(regaxOneLineComment, RegexOptions.IgnoreCase), Color.Green),
+                new KeyValuePair<Regex, Color>(new Regex(regaxMultilineComment, RegexOptions.IgnoreCase), Color.Green),
+                new KeyValuePair<Regex, Color>(new Regex(regaxInvertedСommas, RegexOptions.IgnoreCase), Color.FromArgb(255,122,122)),
+            };
+
+            SetWhiteFontColor(richTextBox1);
+            //При вводе в richtextbox проверь каждый из наборов на совпадение
+            foreach (var element in PAIRS)
+                ColourText(richTextBox1, element.Key, element.Value);
+
+            SetFormateDate(richTextBox1, regaxDate);
+        }
+
+        private void SetWhiteFontColor(RichTextBox rtb)
+        {
+            int pos = rtb.SelectionStart;
+            rtb.Select(5, rtb.SelectionLength);
+            rtb.SelectionColor = Color.White;
+            rtb.DeselectAll();
+            rtb.SelectionStart = pos;
+
+        }
+
+        private void SetFormateDate(RichTextBox rtb, string regEx)
+        {
+            int pos = rtb.SelectionStart;
+            Regex regex = new Regex(regEx);
+            foreach (Match match in regex.Matches(rtb.Text))
+            {
+                if(DateTime.TryParse(match.Value, out DateTime date))
+                {
+                    int posStart = match.Index;
+                    rtb.Text = rtb.Text.Remove(posStart, 10);
+                    rtb.SelectionStart = posStart;
+                    rtb.SelectedText += date.ToString("D");
+                }
+                else
+                {
+                    rtb.SelectionStart = pos;
+                    rtb.SelectionColor = Color.Red;
+                    rtb.SelectedText += " ТАКОЙ ДАТЫ НЕ СУЩЕСТВУЕТ! ПРОВЕРЬТЕ ДАТУ!";
+                    rtb.SelectionColor = Color.White;
+                }
+            }
+        }
+
+        private void ColourText(RichTextBox rtb, Regex regEx, Color col)
+        {
+            int pos = rtb.SelectionStart;
+            foreach (Match match in regEx.Matches(rtb.Text))
+            {
+                rtb.Select(match.Index, match.Length);
+                rtb.SelectionColor = col;
+                rtb.DeselectAll();
+            }
+            rtb.SelectionStart = pos;
+            rtb.SelectionColor = Color.White;
+            
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
